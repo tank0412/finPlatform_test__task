8a4ff8d5c267276f31bc0d0bd77ec953c4f11b21
Roman <romabukhtiarov@gmail.com>
2022-05-27 17:05:44 +0300

    Mark db user and pw as private

--- src/main/java/org/university/DB.java
+++ src/main/java/org/university/DB.java
@@ -7,9 +7,9 @@ import java.time.LocalDate;
 
 public class DB {
     private static final String DB_DRIVER = "org.h2.Driver";
-    public static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-    public static final String DB_USER = "";
-    public static final String DB_PASSWORD = "";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
 
     public void selectAllStudents() throws SQLException {
         Connection connection = getDBConnection();
